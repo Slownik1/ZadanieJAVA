0fe63a196aa717632f9f047a7dc857d344ef1b2d
Dawid Bialek <dawid.bialek@sekonixpoland.com>
2021-10-09 14:37:03 +0200

    ADD Upper method

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,26 @@
 public class Main {
 
+    public static String Upper(String text){
+
+        for(int i=0; i<text.length(); i++){
+
+            int litera = (int)text.charAt(i);
+
+            if((litera>96)&&(litera<122)) {
+
+                litera -= 32;
+
+            }
+
+        }
+
+        return "";
+
+    }
+
     public static void main(String[] args) {
 
-        System.out.println("Hello world ");
+
 
     }
 
