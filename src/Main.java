fc373079004aceb44c3d7f65fe44d5dfd6757c93
Dawid Bialek <dawid.bialek@sekonixpoland.com>
2021-10-09 15:01:09 +0200

    ADD else in IF in Upper method

--- src/Main.java
+++ src/Main.java
@@ -5,22 +5,27 @@ public class Main {
         for(int i=0; i<text.length(); i++){
 
             int litera = (int)text.charAt(i);
+            String text2="";
 
             if((litera>96)&&(litera<122)) {
 
-                litera -= 32;
+                text2=String.valueOf(text.charAt(i));
 
+            }
+            else
+            {
+                text2=String.valueOf(text.charAt(i));
             }
 
         }
 
-        return "";
+        return text;
 
     }
 
     public static void main(String[] args) {
 
-
+        System.out.println(Upper("Test"));
 
     }
 
