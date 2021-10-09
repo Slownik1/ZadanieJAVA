2b231d6a65035105f55be0cfcf2f0fdaa1f60703
D_borok <newen@interia.pl>
2021-10-09 14:35:37 +0200

    test 2 branch_denis

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,16 @@
 public class Main {
 
+    public static String Lower(String tekst){
+        for (int i = 0; i<tekst.length();i++)
+
+
+
+        }
+
+
     public static void main(String[] args) {
 
-        System.out.println("Hello world 5 ");
+
 
 
     }
