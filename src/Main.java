fcfb2c10f2107e934ce35e28126164138c5524d7
D_borok <newen@interia.pl>
2021-10-09 15:01:11 +0200

    for loop aded on branch_Denis

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,13 @@
 public class Main {
 
     public static String Lower(String tekst){
-        for (int i = 0; i<tekst.length();i++)
+        for (int i = 0; i<tekst.length();i++){
+
+            if((tekst.charAt(i))>64 && (tekst.charAt(i)<91)) {
+
+            }
+
+        }
 
 
 
